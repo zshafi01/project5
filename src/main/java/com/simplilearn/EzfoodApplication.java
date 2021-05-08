026bec925ccd81c15a9c46f969ec4949e7f1bd41
zulikha <zshafi01@gmail.com>
2021-05-08 17:58:25 -0500

    comment added

--- src/main/java/com/simplilearn/EzfoodApplication.java
+++ src/main/java/com/simplilearn/EzfoodApplication.java
@@ -3,6 +3,7 @@ package com.simplilearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//github with jenkins configured
 @SpringBootApplication
 public class EzfoodApplication {
 
