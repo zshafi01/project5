529c5c947c390af2ed92fb1b79785c9fe1bec9ca
zulikha <zshafi01@gmail.com>
2021-05-19 19:56:56 -0500

    commente added

--- src/main/java/com/simplilearn/EzfoodApplication.java
+++ src/main/java/com/simplilearn/EzfoodApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //github with jenkins configured
+//I am just commiting to start pipeline
 @SpringBootApplication
 public class EzfoodApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(EzfoodApplication.class, args);
 	}
 
